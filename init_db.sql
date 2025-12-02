a35ec9302273aa01d644000d19fb21ad0f4e9d3b
adamGrgic <argrgic@gmail.com>
2025-12-01 22:38:40 -0700

    small db update

--- init_db.sql
+++ init_db.sql
@@ -5,6 +5,8 @@ $$
 BEGIN
    IF NOT EXISTS (SELECT FROM pg_roles WHERE rolname = 'address_admin') THEN
       CREATE USER address_admin WITH PASSWORD 'admin12345';
+
+      GRANT ALL PRIVILEGES ON DATABASE address_book TO address_admin;
       -- Optional: Grant privileges or set other attributes here
       -- GRANT CONNECT ON DATABASE your_database TO your_username;
       -- GRANT USAGE ON SCHEMA your_schema TO your_username;
